fe3f1f11b3ccdee856cb46bac077f2c84fe553b0
Victor <kosov941@gmail.vom>
2022-10-13 10:57:41 +0700

    UseCases and repository

---
+++ app/src/main/java/com/kosov941/shoppinglist2/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.kosov941.shoppinglist2.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/kosov941/shoppinglist2/domain/ShopItem.kt
@@ -0,0 +1,8 @@
+package com.kosov941.shoppinglist2.domain
+
+data class ShopItem(
+    val id: Int,
+    val name: String,
+    val count: Double,
+    val enabled:Boolean
+)
