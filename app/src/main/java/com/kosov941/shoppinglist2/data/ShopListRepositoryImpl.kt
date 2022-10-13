bd738ddd3be651f939ad4e631a9fd03cd4a4ad02
Victor <kosov941@gmail.vom>
2022-10-13 11:36:46 +0700

    ShopListRepositoryImpl

---
+++ app/src/main/java/com/kosov941/shoppinglist2/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.kosov941.shoppinglist2.data
+
+import com.kosov941.shoppinglist2.domain.ShopItem
+import com.kosov941.shoppinglist2.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private var shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldShopItem = getShopItem(shopItem.id)
+        deleteShopItem(oldShopItem)
+        addShopItem(shopItem)
+
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
