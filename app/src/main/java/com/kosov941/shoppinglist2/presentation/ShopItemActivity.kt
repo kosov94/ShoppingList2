f2cfaeeda4393b55f9bbc32f2445895422eec54f
kosov <kosov941@gmail.com>
2022-11-09 23:15:59 +0700

    start ShopItemActivity

--- app/src/main/java/com/kosov941/shoppinglist2/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/kosov941/shoppinglist2/presentation/ShopItemActivity.kt
@@ -1,5 +1,7 @@
 package com.kosov941.shoppinglist2.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kosov941.shoppinglist2.R
@@ -8,5 +10,25 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_EDIT = "mode_edit"
+
+        fun newIntentAddItem(context: Context): Intent{
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int):Intent{
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID,shopItemId)
+            return intent
+        }
     }
 }
