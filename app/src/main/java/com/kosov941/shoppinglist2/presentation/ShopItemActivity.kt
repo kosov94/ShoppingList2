12b94f53c390a25d9a85c3f2f660f2da3511aa3b
kosov <kosov941@gmail.com>
2022-11-09 09:41:19 +0700

    Create ShopItemActivity

---
+++ app/src/main/java/com/kosov941/shoppinglist2/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.kosov941.shoppinglist2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kosov941.shoppinglist2.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
